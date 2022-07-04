7959da5f983051fb26f0317fcf079608019683fd
PauFugit <paulinefugit@gmail.com>
2022-07-04 16:10:17 +0000

    footer y jumbotron

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,9 @@
+const footer=() => {
+    return(
+        <footer>
+	        <div class="container-fluid bg-dark py-4">
+		        <p class=" text-white text-center fw-lighter" style="font-size:10px;"> Copyright C Your Website 2019</p>
+	        </div>
+        </footer>
+    )
+}
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,11 @@
+const jumbotron=() => {
+    return(
+        <div class="container">
+            <div class="jumbotron pt-1 pb-5">
+    	        <h1 class="display-4"><strong>A Warm Welcome!</strong></h1>
+	            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+	    	    <p class="lead"><a class="btn btn-primary btn-lg" href="#" role="button">Call to action!</a></p>
+        	</div>
+    	</div>
+    )
+}
