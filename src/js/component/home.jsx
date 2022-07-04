7492c081b3e59f91cfdcb40966cc832dc2056c5b
Sussanara <sussanaracely@gmail.com>
2022-07-04 14:58:57 +0000

    Navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white bg-dark ms-3" href="#">Start Bootstrap</a>
+    <div class="collapse navbar-collapse position-absolute top-50 end-0 translate-middle-y me-3">
+      <a class="nav-link text-white bg-dark" href="#"><small>Home</small></a>
+      <a class="nav-link text-white bg-dark" href="#"><small>About</small></a>
+      <a class="nav-link text-white bg-dark" href="#"><small>Services</small></a>
+      <a class="nav-link text-white bg-dark"><small>Contact</small></a>
+    </div>
+  </div>
+</nav>
+
+
 	);
 };
 
