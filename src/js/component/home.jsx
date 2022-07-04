edb4b93cf3ce4298f87e46d8d0fc30a2ca4d3296
Sussanara <sussanaracely@gmail.com>
2022-07-04 21:55:51 +0000

    Props navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-
+import Portfolio from "./Portfolio";
+import Navbar from "./Navbar";
+import Card from "./Card";
 
 //create your first component
 const Home = () => {
-	return (
-		<nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand text-white bg-dark ms-3" href="#">Start Bootstrap</a>
-    <div class="collapse navbar-collapse position-absolute top-50 end-0 translate-middle-y me-3">
-      <a class="nav-link text-white bg-dark" href="#"><small>Home</small></a>
-      <a class="nav-link text-white bg-dark" href="#"><small>About</small></a>
-      <a class="nav-link text-white bg-dark" href="#"><small>Services</small></a>
-      <a class="nav-link text-white bg-dark"><small>Contact</small></a>
-    </div>
-  </div>
-</nav>
+  let text1Navbar = "Start Bootstrap"
+  let text2Navbar = "Home"
+  let text3Navbar = "About"
+  let text4Navbar = "Services"
+  let text5Navbar = "Contact"
 
-
-	);
+  let text6Card = "Card title"
+  let text7Card = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+  return (
+    <>
+	<Navbar text1 ={text1Navbar} text2 ={text2Navbar} text3={text3Navbar} text4={text4Navbar} text5={text5Navbar} />
+  <Portfolio/>
+  <Card text6 = {text6Card} text7= {text7Card}/>
+    </>
+  );
 };
 
 export default Home;
